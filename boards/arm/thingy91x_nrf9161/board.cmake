8d3c9a46f2471c02768ab377b3d31661b42be4e5
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2022-09-05 15:25:05 +0200

    Add thingy91x board
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>
    Signed-off-by: Bernt Johan Damslora <bernt.damslora@nordicsemi.no>
    Co-authored-by: Bernt Johan Damslora <bernt.damslora@nordicsemi.no>

---
+++ boards/arm/thingy91x_nrf9161/board.cmake
@@ -0,0 +1,8 @@
+# Copyright (c) 2019 Nordic Semiconductor ASA.
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+
+board_runner_args(nrfjprog "--softreset")
+board_runner_args(jlink "--device=cortex-m33" "--speed=4000")
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
