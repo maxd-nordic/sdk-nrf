a2f59fcbac2c90427cb49c35608a7de083837b09
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2022-09-21 12:54:30 +0200

    low power mode

--- applications/thingy91v2_gnss_eval/src/app.h
+++ applications/thingy91v2_gnss_eval/src/app.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#if CONFIG_APP_LOW_POWER_MODE
+#define STATE_INIT_COLOR		0b000
+#define STATE_CONNECT_MQTT_COLOR	0b000
+#define STATE_WAIT_FOR_COMMAND_COLOR	0b000
+#define STATE_SEND_RESULTS_COLOR	0b000
+#define STATE_COUNTDOWN_COLOR		0b000
+#define STATE_MEASURE_COLOR		0b000
+#define STATE_MEASURE_WAIT_COLOR	0b000
+#define STATE_GOT_FIX_COLOR		0b000
+#else
 #define STATE_INIT_COLOR		0b111 // white
 #define STATE_CONNECT_MQTT_COLOR	0b110 // cyan
 #define STATE_WAIT_FOR_COMMAND_COLOR	0b010 // green
@@ -8,6 +18,7 @@
 #define STATE_MEASURE_COLOR		0b011 // yellow
 #define STATE_MEASURE_WAIT_COLOR	0b001 // red
 #define STATE_GOT_FIX_COLOR		0b101 // purple
+#endif
 #include <dk_buttons_and_leds.h>
 
 enum state_type {
