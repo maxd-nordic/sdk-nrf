3fc2e15da9fdb10b92e561e3afc026654978a34e
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-09-06 15:10:42 +0200

    fix connectivity bridge strings
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- applications/connectivity_bridge/src/disk/config.c
+++ applications/connectivity_bridge/src/disk/config.c
@@ -109,10 +109,9 @@ static struct cfg_option configs[] = {
 #undef X
 };
 
-// TODO
 static const char file_contents_header[] =
 "==========================================\r\n"
-"  Nordic Thingy:91 Configuration options\r\n"
+"          Configuration options\r\n"
 "==========================================\r\n"
 "The parameters below can be changed at runtime.\r\n"
 "\r\n"
