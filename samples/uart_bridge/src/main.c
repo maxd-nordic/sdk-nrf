c80d520b1860d5754b0ca2a99c6698b4692670c1
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-10-23 16:26:26 +0200

    usb cdc part of uart bridge works now!
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- samples/uart_bridge/src/main.c
+++ samples/uart_bridge/src/main.c
@@ -1,5 +1,6 @@
 #include <zephyr/kernel.h>
 #include <zephyr/drivers/uart.h>
+#include <zephyr/usb/usb_device.h>
 #include <zephyr/sys/ring_buffer.h>
 
 #include <zephyr/logging/log.h>
@@ -12,19 +13,20 @@ struct rx_done_event {
 
 K_MSGQ_DEFINE(rx_queue_0, sizeof(struct rx_done_event), 10, 2);
 K_MSGQ_DEFINE(rx_queue_1, sizeof(struct rx_done_event), 10, 2);
+K_MSGQ_DEFINE(rx_queue_2, sizeof(struct rx_done_event), 10, 2);
 
 /* copying from connectivity bridge */
-static const struct device *devices[] = {
+static const struct device *uart_devices[] = {
 	DEVICE_DT_GET(DT_ALIAS(serial_usb)),
 	DEVICE_DT_GET(DT_ALIAS(serial_uart)),
 };
 
-static struct k_msgq *rx_queues[] = {
+static struct k_msgq *uart_rx_queues[] = {
 	&rx_queue_0,
 	&rx_queue_1,
 };
 
-#define UART_DEVICE_COUNT ARRAY_SIZE(devices)
+#define UART_DEVICE_COUNT ARRAY_SIZE(uart_devices)
 
 #define UART_BUF_SIZE 4096
 
@@ -60,7 +62,6 @@ K_MEM_SLAB_DEFINE(uart_rx_slab, UART_SLAB_BLOCK_SIZE, UART_SLAB_BLOCK_COUNT, UAR
 static struct uart_tx_buf uart_tx_ringbufs[UART_DEVICE_COUNT];
 static uint32_t uart_default_baudrate[UART_DEVICE_COUNT];
 /* UART RX only enabled when there is one or more subscribers (power saving) */
-static int subscriber_count[UART_DEVICE_COUNT];
 static bool enable_rx_retry[UART_DEVICE_COUNT];
 static atomic_t uart_tx_started[UART_DEVICE_COUNT];
 
@@ -137,7 +138,7 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
 			.len = evt->data.rx.len,
 		};
 
-		k_msgq_put(rx_queues[dev_idx], &event, K_NO_WAIT);
+		k_msgq_put(uart_rx_queues[dev_idx], &event, K_NO_WAIT);
 
 		break;
 	case UART_RX_BUF_RELEASED:
@@ -197,7 +198,7 @@ static void uart_callback(const struct device *dev, struct uart_event *evt, void
 
 static void set_uart_baudrate(uint8_t dev_idx, uint32_t baudrate)
 {
-	const struct device *dev = devices[dev_idx];
+	const struct device *dev = uart_devices[dev_idx];
 	struct uart_config cfg;
 	int err;
 
@@ -226,7 +227,7 @@ static void set_uart_baudrate(uint8_t dev_idx, uint32_t baudrate)
 static void set_uart_power_state(uint8_t dev_idx, bool active)
 {
 #if UART_SET_PM_STATE
-	const struct device *dev = devices[dev_idx];
+	const struct device *dev = uart_devices[dev_idx];
 	int err;
 	enum pm_device_action action;
 
@@ -241,7 +242,7 @@ static void set_uart_power_state(uint8_t dev_idx, bool active)
 
 static void enable_uart_rx(uint8_t dev_idx)
 {
-	const struct device *dev = devices[dev_idx];
+	const struct device *dev = uart_devices[dev_idx];
 	int err;
 	struct uart_rx_buf *buf;
 
@@ -267,7 +268,7 @@ static void enable_uart_rx(uint8_t dev_idx)
 
 static void disable_uart_rx(uint8_t dev_idx)
 {
-	const struct device *dev = devices[dev_idx];
+	const struct device *dev = uart_devices[dev_idx];
 	int err;
 
 	err = uart_rx_disable(dev);
@@ -286,7 +287,7 @@ static int uart_tx_start(uint8_t dev_idx)
 	len = ring_buf_get_claim(&uart_tx_ringbufs[dev_idx].rb, &buf,
 				 sizeof(uart_tx_ringbufs[dev_idx].buf));
 
-	err = uart_tx(devices[dev_idx], buf, len, 0);
+	err = uart_tx(uart_devices[dev_idx], buf, len, 0);
 	if (err) {
 		LOG_ERR("uart_tx: %d", err);
 		uart_tx_finish(dev_idx, 0);
@@ -337,23 +338,197 @@ static int uart_tx_enqueue(const uint8_t *data, size_t data_len, uint8_t dev_idx
 
 /* end of copycat */
 
-int main(void)
+/* USB CDC ACM start */
+static const struct device *cdc_devices[] = {
+	DEVICE_DT_GET(DT_NODELABEL(cdc_acm_uart0)),
+};
+#define CDC_DEVICE_COUNT       ARRAY_SIZE(cdc_devices)
+#define USB_CDC_DTR_POLL_MS    500
+#define USB_CDC_RX_BLOCK_SIZE  4096
+#define USB_CDC_RX_BLOCK_COUNT (CDC_DEVICE_COUNT * 3)
+#define USB_CDC_SLAB_ALIGNMENT 4
+
+static void cdc_dtr_timer_handler(struct k_timer *timer);
+static void cdc_dtr_work_handler(struct k_work *work);
+static K_TIMER_DEFINE(cdc_dtr_timer, cdc_dtr_timer_handler, NULL);
+static K_WORK_DEFINE(cdc_dtr_work, cdc_dtr_work_handler);
+/* Incoming data from any CDC instance is copied into a block from this slab */
+K_MEM_SLAB_DEFINE(cdc_rx_slab, USB_CDC_RX_BLOCK_SIZE, USB_CDC_RX_BLOCK_COUNT,
+		  USB_CDC_SLAB_ALIGNMENT);
+
+static uint32_t cdc_ready[CDC_DEVICE_COUNT];
+static uint8_t cdc_overflow_buf[64];
+
+static struct k_msgq *cdc_rx_queues[] = {
+	&rx_queue_2,
+};
+
+static void cdc_dtr_timer_handler(struct k_timer *timer)
 {
-	LOG_INF("Hello World! %s", CONFIG_BOARD);
-	
-	for (int i = 0; i < UART_DEVICE_COUNT; ++i) {
-		struct uart_config cfg;
-		if (!device_is_ready(devices[i])) {
-			LOG_ERR("UART device not ready: %s", devices[i]->name);
+	k_work_submit(&cdc_dtr_work);
+}
+
+/* wait until CDC port has been opened */
+static void cdc_poll_dtr(void)
+{
+	for (int i = 0; i < CDC_DEVICE_COUNT; ++i) {
+		int err;
+		uint32_t cdc_val;
+
+		err = uart_line_ctrl_get(cdc_devices[i], UART_LINE_CTRL_DTR, &cdc_val);
+		if (err) {
+			LOG_WRN("uart_line_ctrl_get(DTR): %d", err);
 			continue;
 		}
-		int err = uart_config_get(devices[i], &cfg);
+
+		if (cdc_val != cdc_ready[i]) {
+			uint32_t baudrate;
+
+			err = uart_line_ctrl_get(cdc_devices[i], UART_LINE_CTRL_BAUD_RATE, &baudrate);
+			if (err) {
+				LOG_WRN("uart_line_ctrl_get(BAUD_RATE): %d", err);
+				continue;
+			}
+
+			cdc_ready[i] = cdc_val;
+		}
+	}
+}
+
+static void cdc_dtr_work_handler(struct k_work *work)
+{
+	cdc_poll_dtr();
+}
+
+static void cdc_uart_interrupt_handler(const struct device *dev, void *user_data)
+{
+	int dev_idx = (int)user_data;
+
+	uart_irq_update(dev);
+
+	while (uart_irq_rx_ready(dev)) {
+		void *rx_buf;
+		int err;
+		int data_length;
+
+		if (cdc_ready[dev_idx] == 0) {
+			/* Peer started sending data before timed COM port state poll */
+			cdc_poll_dtr();
+		}
+
+		err = k_mem_slab_alloc(&cdc_rx_slab, &rx_buf, K_NO_WAIT);
+		if (err) {
+			data_length = uart_fifo_read(dev, cdc_overflow_buf, sizeof(cdc_overflow_buf));
+			LOG_WRN("CDC_%d RX overflow", dev_idx);
+		} else {
+			data_length = uart_fifo_read(dev, rx_buf, USB_CDC_RX_BLOCK_SIZE);
+
+			if (data_length) {
+				struct rx_done_event event = {
+					.buf = rx_buf,
+					.len = data_length,
+				};
+
+				k_msgq_put(cdc_rx_queues[dev_idx], &event, K_NO_WAIT);
+
+			} else {
+				k_mem_slab_free(&cdc_rx_slab, &rx_buf);
+			}
+		}
+		// TODO: disable RX when mem slab is exhausted
+	}
+}
+
+static void cdc_enable_rx_irq(int dev_idx)
+{
+	uart_irq_callback_user_data_set(cdc_devices[dev_idx], cdc_uart_interrupt_handler,
+					(void *)dev_idx);
+	uart_irq_rx_enable(cdc_devices[dev_idx]);
+}
+
+
+static int cdc_tx_enqueue(const uint8_t *data, size_t data_len, uint8_t dev_idx)
+{
+
+	if (dev_idx >= CDC_DEVICE_COUNT) {
+		return false;
+	}
+
+	/* this is not officially allowed to be called outside ISRs.
+	   we'll do it anyway for we are savages. */
+	int written = uart_fifo_fill(
+		cdc_devices[dev_idx],
+		data,
+		data_len
+	);
+
+	if (written == data_len) {
+		return 0;
+	} else {
+		return -ENOMEM;
+	}
+}
+
+static void usbd_status(enum usb_dc_status_code cb_status, const uint8_t *param)
+{
+
+	switch (cb_status) {
+	case USB_DC_ERROR:
+		LOG_ERR("USB_DC_ERROR");
+		break;
+	case USB_DC_RESET:
+		LOG_DBG("USB_DC_RESET");
+		break;
+	case USB_DC_CONNECTED:
+		LOG_DBG("USB_DC_CONNECTED");
+		break;
+	case USB_DC_CONFIGURED:
+		LOG_DBG("USB_DC_CONFIGURED");
+		k_timer_start(&cdc_dtr_timer, K_MSEC(USB_CDC_DTR_POLL_MS), K_MSEC(USB_CDC_DTR_POLL_MS));
+		break;
+	case USB_DC_DISCONNECTED:
+		LOG_DBG("USB_DC_DISCONNECTED");
+		k_timer_stop(&cdc_dtr_timer);
+		break;
+	case USB_DC_SUSPEND:
+		LOG_DBG("USB_DC_SUSPEND");
+		k_timer_stop(&cdc_dtr_timer);
+		break;
+	case USB_DC_RESUME:
+		LOG_DBG("USB_DC_RESUME");
+		break;
+	default:
+		break;
+	}
+}
+
+/* USB CDC ACM end */
+
+int main(void)
+{
+	int err;
+
+	struct k_poll_event poll_evts[3];
+
+	k_poll_event_init(&poll_evts[0], K_POLL_TYPE_MSGQ_DATA_AVAILABLE, K_POLL_MODE_NOTIFY_ONLY, &rx_queue_0);
+	k_poll_event_init(&poll_evts[1], K_POLL_TYPE_MSGQ_DATA_AVAILABLE, K_POLL_MODE_NOTIFY_ONLY, &rx_queue_1);
+	k_poll_event_init(&poll_evts[2], K_POLL_TYPE_MSGQ_DATA_AVAILABLE, K_POLL_MODE_NOTIFY_ONLY, &rx_queue_2);
+
+	LOG_INF("Hello World! %s", CONFIG_BOARD);
+
+	/* initialize UART devices */
+	for (int i = 0; i < UART_DEVICE_COUNT; ++i) {
+		struct uart_config cfg;
+		if (!device_is_ready(uart_devices[i])) {
+			LOG_ERR("UART device not ready: %s", uart_devices[i]->name);
+			continue;
+		}
+		err = uart_config_get(uart_devices[i], &cfg);
 		if (err) {
 			LOG_ERR("uart_config_get: %d", err);
 			return false;
 		}
 		uart_default_baudrate[i] = cfg.baudrate;
-		subscriber_count[i] = 1;
 		enable_rx_retry[i] = false;
 		atomic_set(&uart_tx_started[i], false);
 		ring_buf_init(&uart_tx_ringbufs[i].rb, sizeof(uart_tx_ringbufs[i].buf),
@@ -364,21 +539,40 @@ int main(void)
 		}
 		enable_uart_rx(i);
 	}
-	
-	uart_tx_enqueue("hello world!", 12, 0);
-	uart_tx_enqueue("hello foooo!", 12, 0);
-	uart_tx_enqueue("hello", 5, 0);
-	uart_tx_enqueue("greetings from the other side", 29, 1);
-	
+
+	/* initialize USB CDC */
+	err = usb_enable(NULL);
+	if (err) {
+		LOG_ERR("usb_enable: %d", err);
+		return false;
+	}
+	for (int i = 0; i < CDC_DEVICE_COUNT; ++i) {
+		cdc_ready[i] = 0;
+		if (device_is_ready(cdc_devices[i])) {
+			cdc_enable_rx_irq(i);
+			LOG_DBG("%s available", cdc_devices[i]->name);
+		} else {
+			LOG_ERR("%s not available", cdc_devices[i]->name);
+		}
+	}
+
+	/* event loop */
 	while (true) {
+		struct rx_done_event event = {0};
+
 		for (int i = 0; i < UART_DEVICE_COUNT; ++i) {
-			struct rx_done_event event = {0};
-			if (!k_msgq_get(rx_queues[i], (void *)&event, K_NO_WAIT)) {
-				LOG_INF("UART%d: got message: [%.*s]", i, event.len, event.buf);
+			if (!k_msgq_get(uart_rx_queues[i], (void *)&event, K_NO_WAIT)) {
+				LOG_INF("UART%d->USB: [%.*s]", i, event.len, event.buf);
+				cdc_tx_enqueue(event.buf, event.len, 0);
 				uart_rx_buf_unref(event.buf);
 			}
 		}
-		k_sleep(K_MSEC(1000));
+		if (!k_msgq_get(cdc_rx_queues[0], (void *)&event, K_NO_WAIT)) {
+			LOG_INF("USB->UART0: [%.*s]", event.len, event.buf);
+			uart_tx_enqueue(event.buf, event.len, 0);
+			k_mem_slab_free(&cdc_rx_slab, (void **) &event.buf);
+		}
+		k_poll(&poll_evts, 3, K_FOREVER);
 	}
 
 	return 0;
