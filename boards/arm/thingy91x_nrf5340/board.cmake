256e803bddbad4d55f264c11f6cf7277da18ad8f
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-12-08 13:57:16 +0100

    boards: add thingy91x board
    Signed-off-by: Bernt Johan Damslora <bernt.damslora@nordicsemi.no>
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

---
+++ boards/arm/thingy91x_nrf5340/board.cmake
@@ -0,0 +1,11 @@
+if(BOARD_THINGY91X_NRF5340_CPUAPP OR BOARD_THINGY91X_NRF5340_CPUAPP_NS)
+board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+endif()
+
+if(BOARD_THINGY91X_NRF5340_CPUNET)
+board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+endif()
+
+include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/nrfutil.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
