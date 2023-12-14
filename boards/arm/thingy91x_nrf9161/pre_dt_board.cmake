2fd67d43f305f67cd6c664a5a2b07c9afb2d4c81
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2023-12-14 15:45:44 +0100

    a dozen tiny tweaks from review
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- boards/arm/thingy91x_nrf9161/pre_dt_board.cmake
+++ boards/arm/thingy91x_nrf9161/pre_dt_board.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2021 Nordic Semiconductor ASA
-# SPDX-License-Identifier: Apache-2.0
+# Copyright (c) 2023 Nordic Semiconductor ASA
+# SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
 
 list(APPEND EXTRA_DTC_FLAGS "-Wno-unique_unit_address_if_enabled")
