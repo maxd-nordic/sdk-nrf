117d71f2b969e9b4cfd85c5b2ff77937693d2094
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-01-29 16:57:46 +0100

    wip

---
+++ tests/subsys/net/lib/simple_config/src/main.c
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2024 Nordic Semiconductor ASA
+ *
+ * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
+ */
+
+#include <unity.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include <zephyr/kernel.h>
+
+#include <zephyr/fff.h>
+
+DEFINE_FFF_GLOBALS;
+
+void setUp(void)
+{}
+
+void tearDown(void)
+{}
+
+/* It is required to be added to each test. That is because unity's
+ * main may return nonzero, while zephyr's main currently must
+ * return 0 in all cases (other values are reserved).
+ */
+extern int unity_main(void);
+
+int main(void)
+{
+	/* use the runner from test_runner_generate() */
+	(void)unity_main();
+
+	return 0;
+}
