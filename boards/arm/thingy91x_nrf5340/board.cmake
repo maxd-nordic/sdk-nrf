8df6da7368691e0aadefb3179ca735edf755ea90
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-01-30 13:33:28 +0100

    enforce family option
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- boards/arm/thingy91x_nrf5340/board.cmake
+++ boards/arm/thingy91x_nrf5340/board.cmake
@@ -1,9 +1,11 @@
 if(BOARD_THINGY91X_NRF5340_CPUAPP OR BOARD_THINGY91X_NRF5340_CPUAPP_NS)
 board_runner_args(jlink "--device=nrf5340_xxaa_app" "--speed=4000")
+board_runner_args(nrfjprog "--nrf-family=NRF53")
 endif()
 
 if(BOARD_THINGY91X_NRF5340_CPUNET)
 board_runner_args(jlink "--device=nrf5340_xxaa_net" "--speed=4000")
+board_runner_args(nrfjprog "--nrf-family=NRF53")
 endif()
 
 include(${ZEPHYR_BASE}/boards/common/nrfjprog.board.cmake)
