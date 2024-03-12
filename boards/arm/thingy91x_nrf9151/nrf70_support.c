506e77585e08251ebf58ac9ec161ba4b10485dfa
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-03-12 11:18:51 +0100

    boards: thingy91x_nrf9151: Add wifi optimization features
    This patch adds start and stop functions for the wifi driver
    and fixes handling of the wifi regulators for the Thingy91x.
    
    Signed-off-by: Maximilian Deubel <maximilian.deubel@nordicsemi.no>

--- boards/arm/thingy91x_nrf9151/nrf70_support.c
+++ boards/arm/thingy91x_nrf9151/nrf70_support.c
@@ -14,55 +14,60 @@
 
 LOG_MODULE_REGISTER(thingy91x_board, CONFIG_BOARD_LOG_LEVEL);
 
-static const struct device *reg_wifi = DEVICE_DT_GET(DT_NODELABEL(reg_wifi));
+static const struct device *wifi_regulator = DEVICE_DT_GET(DT_NODELABEL(reg_wifi));
 
 static const struct gpio_dt_spec ldsw_rf_fe_sr_en = {
 	.port = DEVICE_DT_GET(DT_PARENT(DT_NODELABEL(ldsw_rf_fe_sr_en))),
 	.pin = 1,
-	.dt_flags = GPIO_ACTIVE_HIGH, /* this will change with board 0.7.0*/
+	.dt_flags = GPIO_ACTIVE_LOW | GPIO_OPEN_DRAIN | GPIO_PULL_UP,
 };
 
-/* Board-specific wifi startup code to run before the wifi device is started */
-__weak int nrf_wifi_if_zep_start_board(const struct device *dev)
+/* Enable the regulator and set the GPIO pin to enable the RF frontend */
+int nrf_wifi_if_zep_start_board(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 	int ret;
 
-	/* Enable the regulator and set the GPIO pin to enable the RF frontend */
-	ret = regulator_enable(reg_wifi);
+	ret = regulator_enable(wifi_regulator);
 	if (ret) {
-		LOG_ERR("Cannot turn on regulator %s (%d)", reg_wifi->name, ret);
+		LOG_ERR("Cannot turn on regulator %s (%d)", wifi_regulator->name, ret);
 		return ret;
 	}
-	ret = regulator_set_mode(reg_wifi, NPM6001_MODE_PWM);
+	ret = regulator_set_mode(wifi_regulator, NPM6001_MODE_PWM);
 	if (ret) {
-		LOG_ERR("Cannot set mode for regulator %s (%d)", reg_wifi->name, ret);
+		LOG_ERR("Cannot set mode for regulator %s (%d)", wifi_regulator->name, ret);
 		return ret;
 	}
-	gpio_pin_set_dt(&ldsw_rf_fe_sr_en, 1);
+
+	ret = gpio_pin_set_dt(&ldsw_rf_fe_sr_en, 1);
+	if (ret) {
+		LOG_ERR("Cannot set GPIO %s (%d)", ldsw_rf_fe_sr_en.port->name, ret);
+		return ret;
+	}
+
+	/* Wait for the load switch to settle on operating voltage. */
 	k_sleep(K_USEC(300));
 
 	return 0;
 }
 
-/* Board-specific wifi shutdown code to run after the wifi device is stopped */
-__weak int nrf_wifi_if_zep_stop_board(const struct device *dev)
+/* Disable the regulator and set the GPIO pin to disable the RF frontend */
+int nrf_wifi_if_zep_stop_board(const struct device *dev)
 {
 	ARG_UNUSED(dev);
 	int ret;
 
-	/* Disable the regulator and set the GPIO pin to disable the RF frontend */
-	ret = regulator_set_mode(reg_wifi, NPM6001_MODE_HYS);
+	ret = regulator_disable(wifi_regulator);
 	if (ret) {
-		LOG_ERR("Cannot set mode for regulator %s (%d)", reg_wifi->name, ret);
+		LOG_ERR("Cannot turn off regulator %s (%d)", wifi_regulator->name, ret);
 		return ret;
 	}
-	ret = regulator_disable(reg_wifi);
+
+	ret = gpio_pin_set_dt(&ldsw_rf_fe_sr_en, 0);
 	if (ret) {
-		LOG_ERR("Cannot turn off regulator %s (%d)", reg_wifi->name, ret);
+		LOG_ERR("Cannot set GPIO %s (%d)", ldsw_rf_fe_sr_en.port->name, ret);
 		return ret;
 	}
-	gpio_pin_set_dt(&ldsw_rf_fe_sr_en, 0);
 
 	return 0;
 }
