252fe312d0261a14e4dbcdd0fbbf26cc3c069592
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-04-08 13:36:09 +0200

    make double-tap-to-crash selectable independently

--- applications/asset_tracker_v2/src/modules/ui_module.c
+++ applications/asset_tracker_v2/src/modules/ui_module.c
@@ -279,7 +279,7 @@ static void button_handler(uint32_t button_states, uint32_t has_changed)
 
 		APP_EVENT_SUBMIT(ui_module_event);
 
-#if CONFIG_MEMFAULT
+#if CONFIG_UI_DOUBLE_TAP_TO_CRASH
 		/* trigger assert for double tap for demo purposes */
 		static int64_t last_update_ms = 0;
 		if ((k_uptime_get() - last_update_ms) < 1000) {
