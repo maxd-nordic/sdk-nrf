b6d7e7b642dc06c0155fe38e7f965a30ffaa15b2
Maximilian Deubel <maximilian.deubel@nordicsemi.no>
2024-07-29 18:03:07 +0200

    wip: wait_for_flash

--- samples/cellular/at_client/src/main.c
+++ samples/cellular/at_client/src/main.c
@@ -11,6 +11,7 @@
 #include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
+#include <zephyr/drivers/gpio.h>
 
 /* To strictly comply with UART timing, enable external XTAL oscillator */
 void enable_xtal(void)
@@ -22,6 +23,10 @@ void enable_xtal(void)
 	sys_notify_init_spinwait(&cli.notify);
 	(void)onoff_request(clk_mgr, &cli);
 }
+
+const struct device *gpio0 = DEVICE_DT_GET(DT_NODELABEL(gpio0));
+
+#define CHECKERR if (err) {printk("GPIO error: %d\n", err); return err;}
 
 int main(void)
 {
@@ -35,6 +40,28 @@ int main(void)
 		return 0;
 	}
 	enable_xtal();
+
+	printk("Releasing I2C/SPI buses\n");
+
+	err = gpio_pin_configure(gpio0, 29, GPIO_OUTPUT_ACTIVE); CHECKERR; // LED_RED
+
+	err = gpio_pin_configure(gpio0,  8, GPIO_DISCONNECTED); CHECKERR; // SCL
+	err = gpio_pin_configure(gpio0,  9, GPIO_DISCONNECTED); CHECKERR; // SDA
+
+	err = gpio_pin_configure(gpio0, 13, GPIO_DISCONNECTED); CHECKERR; // SCK
+	err = gpio_pin_configure(gpio0, 14, GPIO_DISCONNECTED); CHECKERR; // MOSI
+	err = gpio_pin_configure(gpio0, 15, GPIO_DISCONNECTED); CHECKERR; // MISO
+
+	err = gpio_pin_configure(gpio0, 12, GPIO_DISCONNECTED); CHECKERR; // GD25LE255E CS
+	err = gpio_pin_configure(gpio0, 17, GPIO_DISCONNECTED); CHECKERR; // nRF7002 CS
+	err = gpio_pin_configure(gpio0, 10, GPIO_DISCONNECTED); CHECKERR; // BMI270 CS
+
+	err = gpio_pin_configure(gpio0, 29, GPIO_OUTPUT_INACTIVE); CHECKERR; // LED_RED
+	err = gpio_pin_configure(gpio0, 31, GPIO_OUTPUT_ACTIVE); CHECKERR; // LED_GREEN
+	err = gpio_pin_configure(gpio0, 22, GPIO_OUTPUT_INACTIVE); CHECKERR; // GPIO1 -> nRF53
+
+	printk("I2C/SPI buses released\n");
+
 	printk("Ready\n");
 
 	return 0;
